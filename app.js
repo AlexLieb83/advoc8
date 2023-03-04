b0af116ef6192de35d9619d538b102ba88f20d4a
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-03-03 18:12:16 -0600

    trying

--- app.js
+++ app.js
@@ -15,15 +15,6 @@ dotenv.config({ path: "./config/config.env" });
 // Passport Config
 require("./config/passport")(passport);
 
-connectDB().then(() => {
-  app.listen(
-    PORT,
-    console.log(
-      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`,
-    ),
-  );
-});
-
 const app = express();
 
 // Body Parser
@@ -96,3 +87,12 @@ app.use("/auth", require("./routes/auth"));
 app.use("/accomplishments", require("./routes/accomplishments"));
 
 const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(
+    PORT,
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`,
+    ),
+  );
+});
